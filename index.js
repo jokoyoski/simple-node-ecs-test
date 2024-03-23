121822554be778434c16abcb8a3d12b5eed927b0
joko <joko>
2024-03-23 12:25:22 +0000

    changed the port

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // app.js
 
 const express = require('express');
+require('dotenv').config();
 const app = express();
-const port = 3000; // You can change the port if needed
+const port = 3001; // You can change the port if needed
 
 // Endpoint to return random numbers
 app.get('/api/v1/result', (req, res) => {
   const randomNumber = Math.floor(Math.random() * 100) + 1; // Generates a random number between 1 and 100
-  res.json({ result: randomNumber });
+  res.json({ result: `${randomNumber} ${process?.env?.ENV ?? 'stage'}` });
 });
 
 // Start the server
